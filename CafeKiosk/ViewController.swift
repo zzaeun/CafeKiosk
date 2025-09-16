762e36fc25b06e7066a4f2d4e8f5f7c2dab878ad
zzaeun <leeje57575@gmail.com>
2025-09-16 12:03:30 +0900

    commit with .gitignore applied

---
+++ CafeKiosk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeKiosk
+//
+//  Created by 이정은 on 9/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
