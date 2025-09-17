5fa024589ff6f9f60294855de1f108c1a31900db
zzaeun <leeje57575@gmail.com>
2025-09-17 17:23:25 +0900

    Refactor: 카테고리 별 모든 메뉴들 구현 완료

--- CafeKiosk/FeatMenu.swift
+++ CafeKiosk/FeatMenu.swift
@@ -12,10 +12,11 @@ class FeatMenu: UIViewController {
 
     let appTitleLabel = UILabel()
     let category: UISegmentedControl = {
-        let segmentedControl = UISegmentedControl(items: ["Drink", "Food", "Products"])
+        let segmentedControl = UISegmentedControl(items: ["Drink", "Food", "Product"])
         
         return segmentedControl
     }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +55,84 @@ class FeatMenu: UIViewController {
             $0.width.equalTo(370)
             $0.height.equalTo(32)
         }
+        
+        let menuHstack = menuHstack()
+        view.addSubview(menuHstack)
+        menuHstack.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(category.snp.bottom).offset(24)
+        }
+
     }
 
+    func menu(Image: String, title: String) -> UIStackView {
+        // 배경 동그라미
+        let circleView = UIView()
+        circleView.backgroundColor = UIColor(red: 243/255, green: 240/255, blue: 233/255, alpha: 1)
+        circleView.snp.makeConstraints {
+            $0.width.height.equalTo(100)
+        }
+        circleView.layer.cornerRadius = 50
+        
+        // 이미지
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: Image)
+        imageView.contentMode = .scaleAspectFit
+        
+        circleView.addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalTo(65)
+        }
+        
+        // 타이틀
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.font = .systemFont(ofSize: 12)
+        titleLabel.numberOfLines =  0
+        titleLabel.textAlignment = .center
+        
+        // VStack으로 정렬
+        let Vstack = UIStackView(arrangedSubviews: [circleView, titleLabel])
+        Vstack.axis = .vertical
+        Vstack.alignment = .center
+        Vstack.spacing = 10
+
+        Vstack.snp.makeConstraints{
+            $0.width.equalTo(105)
+            $0.height.equalTo(150)
+        }
+        return Vstack
+    }
+    
+    func menuHstack() -> UIStackView {
+        // Drink
+        let drink1 = menu(Image: "아이스아메리카노", title: "아이스 아메리카노")
+        let drink2 = menu(Image: "자몽허니블랙티", title: "자몽 허니 블랙티")
+        let drink3 = menu(Image: "자바칩프라푸치노", title: "자바칩 프라푸치노")
+        
+        let drinkHstack = UIStackView(arrangedSubviews: [drink1, drink2, drink3])
+        drinkHstack.axis = .horizontal
+        drinkHstack.spacing = 22
+        
+        // Food
+        let food1 = menu(Image: "멜팅치즈베이컨토스트", title: "멜팅 치즈 \n베이컨 토스트")
+        let food2 = menu(Image: "바게트소금빵", title: "바게트 소금빵")
+        let food3 = menu(Image: "진한가나슈9레이어케이크", title: "진한 가나슈 9\n레이어 케이크")
+        
+        let foodHstack = UIStackView(arrangedSubviews: [food1, food2, food3])
+        foodHstack.axis = .horizontal
+        foodHstack.spacing = 22
+        
+        // Product
+        let product1 = menu(Image: "별바당블렌드", title: "별바당 블렌드 250g")
+        let product2 = menu(Image: "시그니처사이렌머그", title: "시그니처 사이렌\n머그 237ml")
+        let product3 = menu(Image: "SS커넥티드콩코드텀블러", title: "SS 커넥티드\n콩코드 텀블러 591ml")
+        
+        let productHstack = UIStackView(arrangedSubviews: [product1, product2, product3])
+        productHstack.axis = .horizontal
+        productHstack.spacing = 22
+        
+        return drinkHstack
+    }
 }
