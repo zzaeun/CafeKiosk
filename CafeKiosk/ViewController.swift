e6a642d1dbc865e643a0433353a2172ba54dd406
zzaeun <leeje57575@gmail.com>
2025-09-19 10:08:18 +0900

    Feat: ViewController에 메뉴 뷰 추가
    forEach를 사용해 addSubview하게 수정 (addSubview 해야하는 양이 많아져서)
    
    func setupUI()에는 UI 담당 코드만 있게 수정
    
    func setConstraints()에는 위치 조절 담당 코드만 있게 수정

--- CafeKiosk/ViewController.swift
+++ CafeKiosk/ViewController.swift
@@ -9,7 +9,14 @@ class ViewController: UIViewController {
     let orderView = FeatOrderView()
     // 메뉴를 표시하는 뷰
     let menuView = FeatMenuView()
-
+    // 앱 제목
+    let appTitleLabel = UILabel()
+    // 메뉴 카테고리
+    let category: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["Drink", "Food", "Product"])
+        segmentedControl.selectedSegmentIndex = 0
+        return segmentedControl
+    }()
     
     // 메뉴title의 key값을 통해 가격과 수량을 저장하는 딕셔너리
     var orders: [String: (price: Int, quantity: Int)] = [:] {
@@ -21,8 +28,10 @@ class ViewController: UIViewController {
     // 테이블 뷰에 표시될 데이터 배열
     var dataArray: [String] = []
     
-    // 음료 메뉴 수평 스택 뷰
+    // 카테고리 별 메뉴 수평 스택 뷰
     lazy var drinkHstack = menuView.menuHstack(items: FeatMenuData.drink)
+    lazy var foodHstack = menuView.menuHstack(items: FeatMenuData.food)
+    lazy var productHstack = menuView.menuHstack(items: FeatMenuData.product)
     
     //로딩 완료 후 호출
     override func viewDidLoad() {
@@ -32,29 +41,35 @@ class ViewController: UIViewController {
 
         setupDelegates()
         setupTargets()
-
+        setConstraints()
+        
+        // 처음 화면에 drinnk 메뉴들만 보이게
+        drinkHstack.isHidden = false
+        foodHstack.isHidden = true
+        productHstack.isHidden = true
+        
+        category.addTarget(self, action: #selector(tappedSegmentedControl), for: .valueChanged)
     }
     
     // 뷰 설정
     func configuerView() {
         view.backgroundColor = .white
-        view.addSubview(orderView)
-        view.addSubview(drinkHstack)
+        [appTitleLabel, category, drinkHstack, foodHstack, productHstack, orderView].forEach {
+            view.addSubview($0)
+        }
     }
     
     //UI설정
     func setupUI(){
-        drinkHstack.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide)
-            make.leading.trailing.equalToSuperview()
-            make.height.equalTo(200)
-        }
+        // appTitleLabel 디자인
+        appTitleLabel.text = "Appsoulte"
+        appTitleLabel.font = .systemFont(ofSize: 25, weight: .semibold)
+        appTitleLabel.textColor = .black
         
-        orderView.snp.makeConstraints { make in
-            make.top.equalTo(drinkHstack.snp.bottom).offset(20)
-            make.leading.trailing.equalToSuperview()
-            make.bottom.equalTo(view.safeAreaLayoutGuide)
-        }
+        // segementedContorl 디자인
+        category.selectedSegmentTintColor = UIColor(red: 0/255, green: 112/255, blue: 74/255, alpha: 1.0)
+        category.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+
     }
     
     // 테이블 뷰 delegate, dataSource 설정
@@ -93,6 +108,59 @@ class ViewController: UIViewController {
             orders[menuName] = existingOrderTuple
         } else {
             orders[menuName] = (price: menuPrice, quantity: 1)
+        }
+    }
+    
+    // 뷰 위치
+    func setConstraints() {
+        // appTitleLabel 위치
+        appTitleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.top.equalToSuperview().offset(87)
+        }
+        
+        // SegmentedControl
+        category.snp.makeConstraints{
+            $0.top.equalTo(appTitleLabel.snp.bottom).offset(18)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(370)
+            $0.height.equalTo(32)
+        }
+        
+        // 메뉴 3개 위치
+        [drinkHstack, foodHstack, productHstack].forEach {
+            $0.snp.makeConstraints {
+                $0.centerX.equalToSuperview()
+                $0.top.equalTo(category.snp.bottom).offset(24)
+            }
+        }
+        
+        // 주문 뷰 위치
+        orderView.snp.makeConstraints { make in
+            make.top.equalTo(drinkHstack.snp.bottom).offset(200)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    // 카테고리 메뉴 선택
+    @objc
+    func tappedSegmentedControl(_ sender: UISegmentedControl) {
+        // 모든 스택 뷰 숨기기
+        drinkHstack.isHidden = true
+        foodHstack.isHidden = true
+        productHstack.isHidden = true
+        
+        // 선택된 스택만 보이게
+        switch sender.selectedSegmentIndex {
+        case 0:
+            drinkHstack.isHidden = false
+        case 1:
+            foodHstack.isHidden = false
+        case 2:
+            productHstack.isHidden = false
+        default:
+            break
         }
     }
     
